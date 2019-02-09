169757c707382bf896cc4eda0fa2635d82ccaa26
yogentamu <harig3307@gmail.com>
2019-02-08 23:36:30 -0600

    Created one new class from Eclipse and pushed.

---
+++ src/test/java/Selenium1/Hello.java
@@ -0,0 +1,14 @@
+package Selenium1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("New Java file");
+		
+		
+	}
+
+}
